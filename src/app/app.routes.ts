0426d52d456b7e2ae9e8966dae579c5fd029539c
el marica de yael <yaelop117@gmail.com>
2023-12-13 11:10:09 -0600

    feat: add basic layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        redirectTo: "auth",
+        pathMatch: "full"
+    },
+    {
+        path: "auth",
+        loadChildren: () => import("./layouts/auth-layout/auth-layout.module").then(m => m.AuthLayoutModule)
+    },
+    {
+        path: "client",
+        loadChildren: () => import("./layouts/client-layout/client-layout.module").then(m => m.ClientLayoutModule)
+    }
+];
---
+++ src/app/layouts/client-layout/client-layout-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ClientLayoutComponent } from './client-layout.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: ClientLayoutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ClientLayoutRoutingModule { }
